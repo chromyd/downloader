0c5cd60a7c90506ac522392e0d456c146ea2be10
chromyd <dusan.chromy@yahoo.de>
2017-12-29 10:15:59 +0100

    Fix issue when stream was already ending in silence. Drop the segment before the first ad-break.

--- finalize.sh
+++ finalize.sh
@@ -19,19 +19,22 @@ echo "Detecting blank commercial breaks..."
 SILENCE_RAW=silence_raw.txt
 SILENCE=silence.txt
 
-ffmpeg -nostats -i $OUTPUT -filter_complex "[0:a]silencedetect=n=-50dB:d=1[outa]" -map [outa] -f s16le -y /dev/null &> $SILENCE_RAW &&
+ffmpeg -nostats -i $OUTPUT -filter_complex "[0:a]silencedetect=n=-50dB:d=10[outa]" -map [outa] -f s16le -y /dev/null &> $SILENCE_RAW &&
 
 grep "^\[silence" $SILENCE_RAW > $SILENCE &&
 
+# If the stream does not end in silence then
 # Add a final silence_start to the slience file, ensuring last segment is kept.
-echo "[silencedetect @ FINAL] silence_start: $(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $OUTPUT)" >> $SILENCE &&
+tail -1 $SILENCE | grep -q silence_end &&
+echo "[silencedetect @ FINAL] silence_start: $(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $OUTPUT)" >> $SILENCE
+
 
 echo "Creating break-free segments..." &&
 
-# Split into segments without ad breaks.
+# Split into segments without ad breaks. Drop the first segment.
 cat $SILENCE | F='-codec copy -v error' FULL_TRACK=$OUTPUT perl -ne '
 INIT { $ss=0; $se=0; }
-  if (/silence_start: (\S+)/) { $ss=$1; $ctr+=1; printf "ffmpeg -nostdin -i $ENV{FULL_TRACK} -ss %f -t %f $ENV{F} -y b_%03d.ts\n", $se, ($ss-$se), $ctr; }
+  if (/silence_start: (\S+)/) { $ss=$1; $ctr+=1; if ($se != 0) { printf "ffmpeg -nostdin -i $ENV{FULL_TRACK} -ss %f -t %f $ENV{F} -y b_%03d.ts\n", $se, ($ss-$se), $ctr; } }
   if (/silence_end: (\S+)/) { $se=$1; }
 '| sh &&
 
@@ -47,6 +50,6 @@ ffmpeg -v error -i $FINAL_TS -c copy -bsf:a aac_adtstoasc $FINAL_MP4 &&
 
 echo "Removing intermediate files..." &&
 
-rm -f *.m3u8 *.key *_*.ts *.txt &&
+rm -f *.m3u8 *.key *_*.ts *raw.txt $FINAL_TS &&
 
 echo Done
